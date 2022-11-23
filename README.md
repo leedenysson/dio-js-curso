6f9185513bedf702c1629448409071fe043e40d5
LeeDenysson <denyssonlee@gmail.com>
2022-11-22 21:33:55 -0300

    remoção arquivos duplicidade 1

--- README.md
+++
@@ -1,2 +0,0 @@
-Modulo 3 - Primeiras Páginas Interativas com JavaScript
-Orange Tech +
--- aula09main.js
+++
@@ -1,7 +0,0 @@
-// const funcoes = require('./aula9aux')
-
-// console.log(funcoes.gets())
-
-const { gets, print} = require('./aula9aux')
-
-print(gets())
